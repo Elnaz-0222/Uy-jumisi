b352e29456f0c9e1ce9b4f8036b9042a0593bd6b
No kredits <ekobili37@gmail.com>
2024-11-06 09:16:15 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -6,14 +6,3 @@ if(age < 18) {
     alert("Сіз ересексіз!")
 }
 
-var temperature = Number(prompt("Сыртқы температураны жазыңыз:"))
-
-if(temperature < 0) {
-    alert("Күн бұлтты жаңбыр немесе қар жауады, қалың киім киуіңізді сұраймын!")
-} else if(temperature > 0 && temperature < 14) {
-    alert("Күн салқын жылы киініңіз!")
-} else if (temperature > 14 && temperature <34) {
-    alert("Жеңіл шортый және фудболка киіңіз өзіңізбен салқын  су алыңыз басыңыздан күн өтіп кетпесін абай болыңыз!")
-}else if(temperature > 34){
-    alert("Далаға шықпауыңызды сұраймын!")
-}
